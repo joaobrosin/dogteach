a58b3a0656ea2249aa9b9d8271b0b5c3ff0ff0db
João Brosin <joao.brosin@gmail.com>
2020-08-16 18:00:33 -0300

    load script and mobile pic fixed

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const dogImage = document.querySelector('.dog-image')
 
+// when the page is loaded
+if (window.innerWidth <= 870) {
+    dogImage.setAttribute('src', 'images/pictures/dog2.png')
+}  else {
+    dogImage.setAttribute('src', 'images/pictures/dog.png')
+}
+
+
+// if the page is resized
 function changePhoto () {
     if (window.innerWidth <= 870) {
         dogImage.setAttribute('src', 'images/pictures/dog2.png')
